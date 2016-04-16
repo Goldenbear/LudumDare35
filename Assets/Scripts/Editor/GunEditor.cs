2149299143adc2e3de3ebe00f940b4ddff42f130
SpencerCongdon <spencer.congdon@gmail.com>
2016-04-16 15:15:34 -0300

    Initial weapons stuff

---
+++ Assets/Scripts/Editor/GunEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Gun), true)]
+public class GunEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if(GUILayout.Button("Fire Weapon"))
+        {
+            (target as Weapon).Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Gun : Weapon
+{
+    [SerializeField, Tooltip("Initial speed of the projectile")]
+    protected float initialSpeed;
+    
+    [SerializeField, Tooltip("Whether or not we aim at a user")]
+    protected bool fireAtEnemy;
+    [SerializeField, Tooltip("Our default shooting direction")]
+    protected Vector3 fireDirection;
+
+
+    public override IEnumerator Fire()
+    {
+        GameObject obj = Instantiate(ProjectilePrefab);
+
+        Projectile proj = obj.GetComponent<Projectile>();
+
+        if(fireAtEnemy)
+        {
+
+        }
+        else
+        {
+            proj.SetInitialVelocity(fireDirection.normalized * initialSpeed);
+        }
+        
+
+        return null;
+    }
+}
