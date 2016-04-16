d809a066d6b2590050c46e86a8bd809758ba7f47
Sean Turner <seanturner123@gmail.com>
2016-04-16 12:46:38 -0300

    Ship base class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A Player ship and its controls
+/// </summary>
+public class Player : Ship 
+{
+	// Public members
+	public int m_playerNumber = 0;
+
+	// Private members
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Abstract base class for all ships.
+/// </summary>
+public abstract class Ship : MonoBehaviour 
+{
+	// What ship shape is a shape shifting ship if the ship's shape shifts
+	public enum ShipShape
+	{
+		k_square,
+		k_circle,
+		k_triangle,
+		k_cross
+	}
+
+	// Public members
+	public ShipShape m_currentShape = ShipShape.k_square;
+
+	// Change ship shape
+	public void ShapeShift(ShipShape newShape)
+	{
+		m_currentShape = newShape;
+	}
+
+	// Private members
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
