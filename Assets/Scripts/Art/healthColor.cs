35b09fd421f137311c1b751486c72b615f80e2f6
Agoras <take_the_bullet_out@hotmail.com>
2016-04-17 18:12:43 -0300

    adding health color ability and player edits

---
+++ Assets/Scripts/Art/healthColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class healthColor : MonoBehaviour {
+
+	public Renderer playerRend;
+
+	public Image image;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		image.color = playerRend.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		image.color = playerRend.material.color;
+	}
+}
